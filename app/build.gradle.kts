d15018dca52c567bd90c04a2c1a2bafdbb9f9c8c
PavlovAlex <al3ksei.pav@yandex.ru>
2024-04-09 15:38:48 +0300

    + ярлык

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pavlovalexey.torpedo"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
