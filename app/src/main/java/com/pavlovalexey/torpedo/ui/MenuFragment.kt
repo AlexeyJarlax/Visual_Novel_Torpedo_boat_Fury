fa15ff62f2a8758bec0a3936b83ecff2389b93cd
PavlovAlex <al3ksei.pav@yandex.ru>
2024-04-13 16:32:37 +0300

    + кнопки меню

--- app/src/main/java/com/pavlovalexey/torpedo/ui/MenuFragment.kt
+++ app/src/main/java/com/pavlovalexey/torpedo/ui/MenuFragment.kt
@@ -9,9 +9,13 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.pavlovalexey.torpedo.R
 
-/** фрагмент для верхнего меню игры. Содержит справку по ресурсам и события, три кнопки*/
+/** фрагмент для верхнего меню игры. Содержит справку по ресурсам и события, кнопки*/
 
 class MenuFragment : Fragment() {
+
+    private fun toast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,20 +25,29 @@ class MenuFragment : Fragment() {
 
         val soundButton = view.findViewById<Button>(R.id.soundButton)
         val autoPlayButton = view.findViewById<Button>(R.id.autoPlayButton)
+        val settingsButton = view.findViewById<Button>(R.id.settingsButton)
         val closeSaveButton = view.findViewById<Button>(R.id.closeSaveButton)
         val menuLayout = view.findViewById<View>(R.id.fragment_menu)
         (activity as MainActivity).hideOptionsLayout()
 
         soundButton.setOnClickListener {
-            Toast.makeText(requireContext(), "Вы нажали кнопку 'Звук'", Toast.LENGTH_SHORT).show()
+            toast("Вы нажали кнопку 'Звук' Функция в разработке")
+            toast("Функция в разработке")
         }
 
         autoPlayButton.setOnClickListener {
-            Toast.makeText(requireContext(), "Вы нажали кнопку 'Автоплей'", Toast.LENGTH_SHORT).show()
+            toast("Вы нажали кнопку 'Автоплей'")
+            toast("Функция в разработке")
+        }
+
+        settingsButton.setOnClickListener {
+            toast("Вы нажали кнопку 'Настройки'")
+            toast("Функция в разработке")
         }
 
         closeSaveButton.setOnClickListener {
-            Toast.makeText(requireContext(), "Вы нажали кнопку 'Закрыть + Сохранить Прогресс'", Toast.LENGTH_SHORT).show()
+            toast("Вы нажали кнопку 'Закрыть + Сохранить Прогресс'")
+            toast("Функция в разработке")
         }
 
         menuLayout.setOnClickListener {
